0a06146944df9df01e10e5ed491e305b4a1d01b1
SACHITHAN <108513425+SACHITHAN@users.noreply.github.com>
2023-03-31 12:42:14 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import FruitSurvey from './Fruit';
+
+function App() {
+  return (
+    <div className="App">
+        <FruitSurvey/>
+    </div>
+  );
+}
+
+export default App;
